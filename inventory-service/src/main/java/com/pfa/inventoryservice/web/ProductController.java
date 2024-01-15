6f8b35eb687f7a1c4803970bdaca14f12211e4c2
hicham <zyadnih@gmail.com>
2024-01-15 14:57:08 +0100

    first commit

---
+++ inventory-service/src/main/java/com/pfa/inventoryservice/web/ProductController.java
@@ -0,0 +1,9 @@
+package com.pfa.inventoryservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public class ProductController {
+
+}
---
+++ inventory-service/src/test/java/com/pfa/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
